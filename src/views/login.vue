d5a6de8892132890fdd58dfa30c814b5139761b6
gaoweijia <934702225@qq.com>
2022-09-05 18:32:42 +0800

    s

--- src/views/login.vue
+++ src/views/login.vue
@@ -2,6 +2,13 @@
 <template>
 	<div class="loginCon">
 		<div class="logo">
+			<img src="../assets/img/logo.png" alt="">
+			<span>遥感信息与图像分析技术国家级重点实验室</span>
+		</div>
+		<div class="earth">
+			<img src="../assets/img/earth.png" alt="">
+		</div>
+		<div class="loginIn">
 			
 		</div>
 	</div>
@@ -21,10 +28,38 @@
 </script>
 
 <style scoped lang="scss">
+	.logo{
+		// border:1px solid red;
+		padding: 20px;
+		img{
+			width:60px;
+		}
+		span{
+			color:#fff;
+			font-size: 20px;
+			font-weight: bold;
+			position: absolute;
+			top:35px;
+			margin-left: 15px;
+		}
+	}
 	.loginCon{
 		width: 100vw;
 		height: 100vh;
 		background-image: url("../assets/img/bgLogin.jpg");
 		background-size: 100% 100%;
+	}
+	.loginIn{
+		background-image: url("../assets/img/loginBg.png");
+	}
+	.earth{
+		// border:1px solid red;
+		width: 40%;
+		position: fixed;
+		top:50%;
+		 transform:translate(10%,-50%);
+		img{
+			width: 100%;
+		}
 	}
 </style>
